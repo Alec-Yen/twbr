67482f0849d8b100d32afb16a192da4158699e94
Yaw Mensah <yaw2000@gmail.com>
2018-11-27 12:44:51 -0500

    Works Kinda

--- imu/Balance.py
+++ imu/Balance.py
@@ -1,6 +1,61 @@
-from simple_imu import read_raw_data
+from simple_imu import read_raw_data, MPU_Init
 from time import sleep
+import  RPi.GPIO as GPIO
+import time
+import smbus
 
-#read_raw_date(Device_Address)
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12,GPIO.OUT)
+pwm=GPIO.PWM(12,100)
+
+
+bus = smbus.SMBus(1)
+Device_Address = 0x68
+
+MPU_Init()
+
+while True:
+	
+	#Read Accelerometer raw value
+	acc_x = read_raw_data(ACCEL_XOUT_H)
+	acc_y = read_raw_data(ACCEL_YOUT_H)
+	acc_z = read_raw_data(ACCEL_ZOUT_H)
+	
+	#Read Gyroscope raw value
+	gyro_x = read_raw_data(GYRO_XOUT_H)
+	gyro_y = read_raw_data(GYRO_YOUT_H)
+	gyro_z = read_raw_data(GYRO_ZOUT_H)
+	
+	#Full scale range +/- 250 degree/C as per sensitivity scale factor
+	Ax = acc_x/16384.0
+	Ay = acc_y/16384.0
+	Az = acc_z/16384.0
+	
+	Gx = gyro_x/131.0
+	Gy = gyro_y/131.0
+	Gz = gyro_z/131.0
+
+        while(Gx < 0):
+	    gyro_x = read_raw_data(GYRO_XOUT_H)
+            Gx = gyro_x/131.0
+            pwm.start(20)
+
+
+
 
 
