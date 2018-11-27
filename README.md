3388ab2bc2d702e9dbd2d517398bc75369dc3e4e
Alec Yen <yenalec@gmail.com>
2018-11-27 17:46:42 -0500

    figured out directions for motor driver

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Two-Wheel Balancing Robot
 
 Classic application from control theory of the inverted pendulum.
+
+IMU cpp: https://github.com/jrowberg/i2cdevlib
+Motor Driver cpp: https://github.com/Steve136/RasPi-DC-Motor-Class
--- imu/python/Motor_Control.py
+++ imu/python/Motor_Control.py
@@ -1,8 +1,26 @@
 import  RPi.GPIO as GPIO
 import time 
+
+PWM_ = 12
+DIR = 16
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.OUT)
-pwm=GPIO.PWM(12,100)
+GPIO.setup(PWM_,GPIO.OUT)
+GPIO.setup(DIR,GPIO.OUT)
+
+GPIO.output(DIR,True)
+pwm=GPIO.PWM(PWM_,100)
 pwm.start(20)
-time.sleep(10)
+time.sleep(2)
+pwm.stop()
+
+
+time.sleep(3)
+
+GPIO.output(DIR,False)
+
+
+pwm=GPIO.PWM(PWM_,100)
+pwm.start(10)
+time.sleep(2)
 pwm.stop()
