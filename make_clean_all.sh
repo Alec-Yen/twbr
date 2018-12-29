21e40968f2fdccfd3a312cc861f9533081ac5181
Alec Yen <yenalec@gmail.com>
2018-12-29 11:19:37 -0500

    added build

--- make_clean_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd balance_mdd10a && make clean && \
-cd ../balance_roboclaw && make clean && \
-cd ../mpu6050_bcm2835 && make clean && \
-cd ../mpu6050_i2c && make clean && \
-cd ../mdd10a && make clean && \
-cd ../roboclaw && make clean
